a91e4ee069d69f997ab5a9494a122a47700cff2d
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2022-03-13 21:36:52 +0200

    Add files via upload

---
+++ Lab3 - Open-Close-Principle/CoffeeMachines/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 BasicCoffeeMachine bcm = new BasicCoffeeMachine(150, 150);
+	 PremiumCoffeeMachine pcm = new PremiumCoffeeMachine(100, 200, 100, 100);
+
+     bcm.perform();
+    }
+}
